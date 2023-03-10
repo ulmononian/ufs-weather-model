a8992a9db90b6dc391a8cac55b242ca30eabe51b
Cameron Book <43379611+ulmononian@users.noreply.github.com>
2023-03-09 21:22:01 -0800

    Update ufs_orion.intel.lua

--- modulefiles/ufs_orion.intel.lua
+++ modulefiles/ufs_orion.intel.lua
@@ -5,18 +5,17 @@ loads UFS Model prerequisites for Orion/Intel
 load("contrib")
 load("noaatools")
 
-prepend_path("MODULEPATH", "/work/noaa/epic-ps/role-epic-ps/miniconda3/modulefiles")
-miniconda3_ver=os.getenv("miniconda3_ver") or "4.12.0"
-load(pathJoin("miniconda3", miniconda3_ver))
-
 prepend_path("MODULEPATH", "/work2/noaa/da/role-da/spack-stack-feature-r2d2-mysql/envs/unified-4.0.0-rc1/install/modulefiles/Core")
 
+spack_intel_ver=os.getenv("spack_intel_ver") or "2022.0.2"
+load(pathJoin("stack-intel", spack_intel_ver))
 
-hpc_intel_ver=os.getenv("hpc_intel_ver") or "2022.0.2"
-load(pathJoin("stack-intel", hpc_intel_ver))
+spack_impi_ver=os.getenv("spack_impi_ver") or "2021.5.1"
+load(pathJoin("stack-intel-oneapi-mpi", spack_impi_ver))
 
-hpc_impi_ver=os.getenv("hpc_impi_ver") or "2021.5.1"
-load(pathJoin("stack-intel-oneapi-mpi", hpc_impi_ver))
+prepend_path("MODULEPATH", "/work/noaa/da/role-da/spack-stack/modulefiles")
+miniconda3_ver=os.getenv("miniconda3_ver") or "3.9.7"
+load(pathJoin("stack-python", miniconda3_ver))
 
 load("ufs_common_spack")
 
