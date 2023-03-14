195f5b6508d640e4ef3cb5b55feb8f9c5e7be912
Cameron Book <bcameron@cheyenne2.cheyenne.ucar.edu>
2023-03-14 10:53:03 -0600

    Add Cheyenne modulefiles; add ufs_common_spack_debug modulefile; update ufs_common_spack.

--- modulefiles/ufs_common_spack_debug.lua
+++ modulefiles/ufs_common_spack_debug.lua
@@ -2,7 +2,7 @@ help([[
 loads UFS Model common libraries
 ]])
 
-load(pathJoin("ufs-weather-model-env", "unified-dev"))
+load(pathJoin("ufs-weather-model-env", "unified-dev-debug"))
 
 w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
 load(pathJoin("w3emc", w3emc_ver))
