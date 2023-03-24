9d9c037e41b1238c08a6a5bf41f7a458e1995cf7
Cameron Book <43379611+ulmononian@users.noreply.github.com>
2023-03-24 09:23:04 -0700

    Update ufs_noaacloud.intel.lua

--- modulefiles/ufs_noaacloud.intel.lua
+++ modulefiles/ufs_noaacloud.intel.lua
@@ -7,7 +7,7 @@ prepend_path("MODULEPATH", "/contrib/Cameron.Book/sw/spack-stack-1.2.0/envs/unif
 
 load("stack-intel")
 load("stack-intel-oneapi-mpi")
-load("cmake/3.22.1"
+load("cmake/3.22.1")
 
 load("ufs_common_spack")
 
