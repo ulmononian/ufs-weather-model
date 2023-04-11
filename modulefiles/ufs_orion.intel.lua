8596d13126d0dfe26e3d7b1de290a5a7d669bc1f
ulmononian <cameron_book@alumni.brown.edu>
2023-04-11 11:36:21 -0500

    Update Orion modulefile to use spack-stack; add ufs_common_spack.

--- modulefiles/ufs_orion.intel.lua
+++ modulefiles/ufs_orion.intel.lua
@@ -2,28 +2,20 @@ help([[
 loads UFS Model prerequisites for Orion/Intel
 ]])
 
-load("contrib")
-load("noaatools")
+prepend_path("MODULEPATH", "/work/noaa/epic-ps/role-epic-ps/spack-stack/spack-stack-1.3.0/envs/unified-env/install/modulefiles/Core")
 
-cmake_ver=os.getenv("cmake_ver") or "3.22.1"
-load(pathJoin("cmake", cmake_ver))
+spack_intel_ver=os.getenv("spack_intel_ver") or "2022.0.2"
+load(pathJoin("stack-intel", spack_intel_ver))
 
-prepend_path("MODULEPATH", "/work/noaa/epic-ps/role-epic-ps/miniconda3/modulefiles")
-miniconda3_ver=os.getenv("miniconda3_ver") or "4.12.0"
-load(pathJoin("miniconda3", miniconda3_ver))
+spack_impi_ver=os.getenv("spack_impi_ver") or "2021.5.1"
+load(pathJoin("stack-intel-oneapi-mpi", spack_impi_ver))
 
-prepend_path("MODULEPATH", "/work/noaa/epic-ps/role-epic-ps/hpc-stack/libs/intel-2022.1.2/modulefiles/stack")
+prepend_path("MODULEPATH", "/work/noaa/da/role-da/spack-stack/modulefiles")
 
-hpc_ver=os.getenv("hpc_ver") or "1.2.0"
-load(pathJoin("hpc", hpc_ver))
+miniconda3_ver=os.getenv("miniconda3_ver") or "3.9.7"
+load(pathJoin("stack-python", miniconda3_ver))
 
-hpc_intel_ver=os.getenv("hpc_intel_ver") or "2022.1.2"
-load(pathJoin("hpc-intel", hpc_intel_ver))
-
-hpc_impi_ver=os.getenv("hpc_impi_ver") or "2022.1.2"
-load(pathJoin("hpc-impi", hpc_impi_ver))
-
-load("ufs_common")
+load("ufs_common_spack")
 
 setenv("CC", "mpiicc")
 setenv("CXX", "mpiicpc")
