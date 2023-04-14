a8e99413f5779ad0da8269d6d609851b8784a818
ulmononian <cameron_book@alumni.brown.edu>
2023-04-13 23:42:14 -0500

    Update Gaea modulefile.

--- modulefiles/ufs_gaea.intel.lua
+++ modulefiles/ufs_gaea.intel.lua
@@ -1,22 +1,24 @@
 help([[
-  This module loads libraries required for building and running UFS Weather Model 
-  on the NOAA RDHPC machine Gaea using Intel-2022.1.2
+  This module loads libraries required for building and running UFS Weather Model
+  on the NOAA RDHPC machine Gaea using Intel-2021.3.0
 ]])
 
 whatis([===[Loads libraries needed for building the UFS Weather Model on Gaea ]===])
 
-prepend_path("MODULEPATH", "/lustre/f2/dev/role.epic/contrib/modulefiles")
-load(pathJoin("miniconda3",os.getenv("miniconda_ver") or "4.12.0"))
+prepend_path("MODULEPATH", "/lustre/f2/dev/wpo/role.epic/contrib/spack-stack/spack-stack-1.3.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/lustre/f2/pdata/esrl/gsd/spack-stack/modulefiles")
 
-load(pathJoin("cmake", os.getenv("cmake_ver") or "3.20.1"))
+stack_intel_ver=os.getenv("stack_intel_ver") or "2021.3.0"
+load(pathJoin("stack-intel", stack_intel_ver))
 
-prepend_path("MODULEPATH","/lustre/f2/dev/role.epic/contrib/hpc-stack/intel-2021.3.0_noarch/modulefiles/stack")
-load(pathJoin("hpc", os.getenv("hpc_ver") or "1.2.0"))
-load(pathJoin("intel", os.getenv("intel_ver") or "2021.3.0"))
-load(pathJoin("hpc-intel", os.getenv("hpc_intel_ver") or "2021.3.0"))
-load(pathJoin("hpc-cray-mpich", os.getenv("hpc_cray_mpich_ver") or "7.7.11"))
-load(pathJoin("gcc", os.getenv("gcc_ver") or "8.3.0"))
-load(pathJoin("libpng", os.getenv("libpng_ver") or "1.6.37"))
+stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "7.7.11"
+load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
+
+stack_python_ver=os.getenv("stack_python_ver") or "3.9.12"
+load(pathJoin("stack-python", stack_python_ver))
+
+cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+load(pathJoin("cmake", cmake_ver))
 
 -- needed for WW3 build
 load(pathJoin("gcc", os.getenv("gcc_ver") or "8.3.0"))
