4c272cacc752c718d6cae939b3bf5f5db2f9c6f7
Alex Richert <alexander.richert@noaa.gov>
2023-04-26 12:01:42 -0700

    Update acorn modules

--- modulefiles/ufs_acorn.intel.lua
+++ modulefiles/ufs_acorn.intel.lua
@@ -8,10 +8,12 @@ load("stack-intel")
 load("stack-cray-mpich")
 load("stack-python")
 
---Avoid conflicts for several packages by unusing system spack installation:
+--Avoid production installations; use spack-stack only:
 remove_path("MODULEPATH", "/apps/ops/prod/libs/modulefiles/compiler/intel/19.1.3.304")
 remove_path("MODULEPATH", "/apps/ops/prod/libs/modulefiles/mpi/intel/19.1.3.304/cray-mpich/8.1.4")
 remove_path("MODULEPATH", "/apps/ops/prod/libs/modulefiles/mpi/intel/19.1.3.304/cray-mpich/8.1.7")
+remove_path("MODULEPATH", "/apps/prod/lmodules/intel/19.1.3.304")
+remove_path("MODULEPATH", "/apps/prod/lmodules/INTEL_cray_mpich/19.1.3.304/cray-mpich/8.1.4")
 
 load("ufs_common")
 
